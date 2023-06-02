d26ac3d1d36a245e25986cf926ce7fdd32a22cfa
atolanrewaju2022 <135293313+atolanrewaju2022@users.noreply.github.com>
2023-06-02 14:53:37 -0500

    Update infracost.sh

--- us-east-1/user-mgmt-app/environments/dev/infracost.sh
+++ us-east-1/user-mgmt-app/environments/dev/infracost.sh
@@ -3,5 +3,5 @@
 terraform init
 terraform plan -out tfplan.binary
 terraform show -json tfplan.binary > plan.json
-export INFRACOST_API_KEY=ico-pXyHSooFPKX2CxeDLt9OSMhjHdxgGCTJ
+export INFRACOST_API_KEY=ico-pXyHSooFPKX2CxeDLt9OSMhjHdxgGCFF
 infracost breakdown --path plan.json
